6466475316e28587cf34d1955d769cfde7a57172
Esben Aarseth <Esben.Aarseth@bekk.no>
2018-06-11 20:01:59 +0200

    Attendees is fun

--- index.js
+++ index.js
@@ -19,16 +19,34 @@ let deltakerId = 0;
 // {navn: "Esben Aarseth", kamp: "id", id: "id"}
 let deltakelser = [];
 
-const addDeltakereToMatch = (match) => {
+const rootUrl = process.env.ROOT_URL || 'http://localhost:' + (process.env.PORT || 3001);
+
+const decorateDeltakelseWithLinks = (match, deltakelse) => {
+    return {
+        navn: deltakelse.navn,
+        _links: {
+            "removeFromAttendees": {
+                "href": `${rootUrl}/api/attendees/${deltakelse.id}`,
+                "method": 'DELETE',
+                "accepts": 'application/json'
+            },
+            "attendeesToSameGame": {
+                "href": `${rootUrl}/api/matches/attendees/${deltakelse.kamp}`,
+                "method": 'GET',
+                "accepts": 'application/json'
+            }
+        }
+    }
+};
+
+const addAttendeesToMatch = (match) => {
     return Object.assign(
         {
-            deltakere: deltakelser
+            attendees: deltakelser
                 .filter(deltakelse => deltakelse.kamp === match.name)
                 .map(deltakelse => {
-                return {
-                    navn: deltakelse.navn,
-                }
-            })
+                    return decorateDeltakelseWithLinks(match, deltakelse);
+                })
         },
         match
     )
@@ -38,12 +56,12 @@ const addDeltakereToMatch = (match) => {
 
 const matches = () => {
     let groupMatches = Object.values(worldcupData.groups).reduce(
-        (allMatches, group) => allMatches.concat(Object.values(group.matches)).map(addDeltakereToMatch),
+        (allMatches, group) => allMatches.concat(Object.values(group.matches)).map(addAttendeesToMatch),
         []
     );
 
     let knockoutMatches = Object.values(worldcupData.knockout).reduce(
-        (allMatches, round) => allMatches.concat(Object.values(round.matches)).map(addDeltakereToMatch),
+        (allMatches, round) => allMatches.concat(Object.values(round.matches)).map(addAttendeesToMatch),
         []
     );
 
@@ -56,24 +74,35 @@ app.get("/api/matches", (req, res) => {
     });
 });
 
-app.get('/api/deltakere/:matchId', (req, res) => {
-    res.send(matches().filter(match => match.name === req.params.matchId).deltakere);
+app.get('/api/matches/attendees/:matchId', (req, res) => {
+    var matchId = req.params.matchId;
+    res.send(matches().filter(match => match.name == matchId).map(match => match.attendees));
 });
 
-app.post('/api/deltakelser', (req, res) => {
+app.post('/api/attendees', (req, res) => {
     const body = req.body;
     if (typeof body.navn === 'undefined') {
+        console.log('Deltaker er udefinert');
         res.status(400).send({message: 'deltaker must be defined'});
     } else if (typeof body.kamp === 'undefined') {
+        console.log('Kamp er udefinert');
         res.status(400).send({message: 'kamp must be defined'});
+    } else if (typeof matches().find(match => match.name === body.kamp) === 'undefined') {
+        console.log('Kunne ikke finne kamp med id ' + body.kamp);
+        res.status(400).send({message: `Failed to find kamp with id ${body.kamp}`});
     }
 
-    deltakelser.push({navn: body.navn, kamp: body.kamp});
-    console.log(deltakelser);
-    res.status(200).send({deltakelsesId: deltakerId++});
+    const confirmation = {navn: body.navn, kamp: body.kamp, id: deltakerId++};
+    deltakelser.push(confirmation);
+
+    res.status(200).send(confirmation);
 });
 
-app.delete('/api/deltakere/:id', (req, res) => {
+app.delete('/api/attendees/:id', (req, res) => {
+    if(typeof deltakelser.find(deltakelse => deltakelse.id === req.params.id) === 'undefined') {
+        res.status(400).send({message: 'Could not find attendance with id: ' + req.params.id});
+        return;
+    };
     deltakelser = deltakelser.filter(deltakelse => deltakelse.id === req.params.id);
     res.status(204).send();
 });
